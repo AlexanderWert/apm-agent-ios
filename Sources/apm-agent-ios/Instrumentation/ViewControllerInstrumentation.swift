2d2ed19dc06c10a855aa9a424318d354a27081fd
Alexander Wert <alexander.wert@elastic.co>
2022-07-08 15:17:53 +0200

    Update ViewControllerInstrumentation.swift

--- Sources/apm-agent-ios/Instrumentation/ViewControllerInstrumentation.swift
+++ Sources/apm-agent-ios/Instrumentation/ViewControllerInstrumentation.swift
@@ -64,51 +64,51 @@
             OpenTelemetrySDK.instance.tracerProvider.get(instrumentationName: "UIViewController", instrumentationVersion: "0.0.1") as! TracerSdk
         }
 
-        class LoadView: MethodSwizzler<
-        @convention(c) (UIViewController, Selector) -> Void,
-            @convention(block) (UIViewController) -> Void
-            >
-            {
-                static func build() throws -> LoadView {
-                    try LoadView(selector: #selector(UIViewController.loadView), klass: UIViewController.self)
-                }
-
-                func swizzle() {
-                    swap { previousImplementation -> BlockSignature in
-                        { viewController -> Void in
-                            let name = "\(type(of: viewController)).loadView()"
-                            _ = MyTraceLogger.startTrace(tracer: ViewControllerInstrumentation.getTracer(), associatedObject: viewController, name: name, isRoot: true)
-
-                            previousImplementation(viewController, self.selector)
-
-                            MyTraceLogger.stopTrace(associatedObject: viewController)
-                        }
-                    }
-                }
-            }
-        
-        class LoadViewIfNeeded: MethodSwizzler<
-        @convention(c) (UIViewController, Selector) -> Void,
-            @convention(block) (UIViewController) -> Void
-            >
-            {
-                static func build() throws -> LoadViewIfNeeded {
-                    try LoadViewIfNeeded(selector: #selector(UIViewController.loadViewIfNeeded), klass: UIViewController.self)
-                }
-
-                func swizzle() {
-                    swap { previousImplementation -> BlockSignature in
-                        { viewController -> Void in
-                            let name = "\(type(of: viewController)).loadViewIfNeeded()"
-                            _ = MyTraceLogger.startTrace(tracer: ViewControllerInstrumentation.getTracer(), associatedObject: viewController, name: name, isRoot: true)
-
-                            previousImplementation(viewController, self.selector)
-
-                            MyTraceLogger.stopTrace(associatedObject: viewController)
-                        }
-                    }
-                }
-            }
+//        class LoadView: MethodSwizzler<
+//        @convention(c) (UIViewController, Selector) -> Void,
+//            @convention(block) (UIViewController) -> Void
+//            >
+//            {
+//                static func build() throws -> LoadView {
+//                    try LoadView(selector: #selector(UIViewController.loadView), klass: UIViewController.self)
+//                }
+//
+//                func swizzle() {
+//                    swap { previousImplementation -> BlockSignature in
+//                        { viewController -> Void in
+//                            let name = "\(type(of: viewController)).loadView()"
+//                            _ = MyTraceLogger.startTrace(tracer: ViewControllerInstrumentation.getTracer(), associatedObject: viewController, name: name, isRoot: true)
+//
+//                            previousImplementation(viewController, self.selector)
+//
+//                            MyTraceLogger.stopTrace(associatedObject: viewController)
+//                        }
+//                    }
+//                }
+//            }
+//
+//        class LoadViewIfNeeded: MethodSwizzler<
+//        @convention(c) (UIViewController, Selector) -> Void,
+//            @convention(block) (UIViewController) -> Void
+//            >
+//            {
+//                static func build() throws -> LoadViewIfNeeded {
+//                    try LoadViewIfNeeded(selector: #selector(UIViewController.loadViewIfNeeded), klass: UIViewController.self)
+//                }
+//
+//                func swizzle() {
+//                    swap { previousImplementation -> BlockSignature in
+//                        { viewController -> Void in
+//                            let name = "\(type(of: viewController)).loadViewIfNeeded()"
+//                            _ = MyTraceLogger.startTrace(tracer: ViewControllerInstrumentation.getTracer(), associatedObject: viewController, name: name, isRoot: true)
+//
+//                            previousImplementation(viewController, self.selector)
+//
+//                            MyTraceLogger.stopTrace(associatedObject: viewController)
+//                        }
+//                    }
+//                }
+//            }
 
         class ViewDidLoad: MethodSwizzler<
         @convention(c) (UIViewController, Selector) -> Void, // IMPSignature
@@ -122,7 +122,10 @@
                 func swizzle() {
                     swap { previousImplementation -> BlockSignature in
                         { viewController -> Void in
-                            let name = "\(type(of: viewController)) - view appearing"
+                            let title = viewController.navigationItem.title
+                            let name = title != nil ? title! : "\(type(of: viewController)) - view appearing"
+                            
+                            
                             _ = TraceLogger.startTrace(tracer: ViewControllerInstrumentation.getTracer(), associatedObject: viewController, name: name)
                             previousImplementation(viewController, self.selector)
                         }
@@ -130,47 +133,47 @@
                 }
             }
 
-        class AddChild: MethodSwizzler<
-        @convention(c) (UIViewController, Selector, UIViewController) -> Void,
-            @convention(block) (UIViewController, UIViewController) -> Void
-        >{
-            static func build() throws -> AddChild {
-                try AddChild(selector: #selector(UIViewController.addChild), klass: UIViewController.self)
-            }
-
-            func swizzle() {
-                swap { previousImplementation -> BlockSignature in
-                    { viewController, child -> Void in
-                        let name = "\(type(of: child)) added to \(type(of: viewController))"
-                        _ = MyTraceLogger.startTrace(tracer: ViewControllerInstrumentation.getTracer(), associatedObject: viewController, name: name, isRoot: false)
-                        previousImplementation(viewController, self.selector, child)
-                        MyTraceLogger.stopTrace(associatedObject: viewController)
-                    }
-                }
-            }
-        }
-
-        class Transition: MethodSwizzler <
-        @convention(c) (UIViewController, Selector, UIViewController, UIViewController, TimeInterval, UIView.AnimationOptions, (() -> Void)?, ((Bool) -> Void)?) -> Void,
-            @convention(block) (UIViewController, UIViewController, UIViewController, TimeInterval, UIView.AnimationOptions, (() -> Void)?, ((Bool) -> Void)?) -> Void
-
-            >
-            {
-                static func build() throws -> Transition {
-                    try Transition(selector: #selector(UIViewController.transition), klass: UIViewController.self)
-                }
-
-                func swizzle() {
-                    swap { previousImplementaion -> BlockSignature in
-                        { viewController, from, to, duration, options, animations, completion -> Void in
-                            let name = "\(type(of: viewController)) transitioning to \(type(of: to)) from \(type(of: from))"
-                            _ = MyTraceLogger.startTrace(tracer: ViewControllerInstrumentation.getTracer(), associatedObject: viewController, name: name, isRoot: true)
-                            previousImplementaion(viewController, self.selector, from, to, duration, options, animations, completion)
-                            MyTraceLogger.stopTrace(associatedObject: viewController)
-                        }
-                    }
-                }
-            }
+//        class AddChild: MethodSwizzler<
+//        @convention(c) (UIViewController, Selector, UIViewController) -> Void,
+//            @convention(block) (UIViewController, UIViewController) -> Void
+//        >{
+//            static func build() throws -> AddChild {
+//                try AddChild(selector: #selector(UIViewController.addChild), klass: UIViewController.self)
+//            }
+//
+//            func swizzle() {
+//                swap { previousImplementation -> BlockSignature in
+//                    { viewController, child -> Void in
+//                        let name = "\(type(of: child)) added to \(type(of: viewController))"
+//                        _ = MyTraceLogger.startTrace(tracer: ViewControllerInstrumentation.getTracer(), associatedObject: viewController, name: name, isRoot: false)
+//                        previousImplementation(viewController, self.selector, child)
+//                        MyTraceLogger.stopTrace(associatedObject: viewController)
+//                    }
+//                }
+//            }
+//        }
+//
+//        class Transition: MethodSwizzler <
+//        @convention(c) (UIViewController, Selector, UIViewController, UIViewController, TimeInterval, UIView.AnimationOptions, (() -> Void)?, ((Bool) -> Void)?) -> Void,
+//            @convention(block) (UIViewController, UIViewController, UIViewController, TimeInterval, UIView.AnimationOptions, (() -> Void)?, ((Bool) -> Void)?) -> Void
+//
+//            >
+//            {
+//                static func build() throws -> Transition {
+//                    try Transition(selector: #selector(UIViewController.transition), klass: UIViewController.self)
+//                }
+//
+//                func swizzle() {
+//                    swap { previousImplementaion -> BlockSignature in
+//                        { viewController, from, to, duration, options, animations, completion -> Void in
+//                            let name = "\(type(of: viewController)) transitioning to \(type(of: to)) from \(type(of: from))"
+//                            _ = MyTraceLogger.startTrace(tracer: ViewControllerInstrumentation.getTracer(), associatedObject: viewController, name: name, isRoot: true)
+//                            previousImplementaion(viewController, self.selector, from, to, duration, options, animations, completion)
+//                            MyTraceLogger.stopTrace(associatedObject: viewController)
+//                        }
+//                    }
+//                }
+//            }
 
         class ViewWillAppear: MethodSwizzler<
         @convention(c) (UIViewController, Selector, Bool) -> Void,
@@ -184,7 +187,8 @@
                 func swizzle() {
                     swap { previousImplementation -> BlockSignature in
                         { viewController, animated -> Void in
-                            let name = "\(type(of: viewController)) - view appearing"
+                            let title = viewController.navigationItem.title
+                            let name = title != nil ? title! : "\(type(of: viewController)) - view appearing"
 
                             _ = TraceLogger.startTrace(tracer: ViewControllerInstrumentation.getTracer(), associatedObject: viewController, name: name)
 
@@ -215,82 +219,82 @@
                 }
             }
 
-        class ViewDidDisappear: MethodSwizzler<
-        @convention(c) (UIViewController, Selector, Bool) -> Void, // IMPSignature
-            @convention(block) (UIViewController, Bool) -> Void // BlockSignature
-            >
-            {
-                static func build() throws -> ViewDidDisappear {
-                    try ViewDidDisappear(selector: #selector(UIViewController.viewDidDisappear), klass: UIViewController.self)
-                }
-
-                func swizzle() {
-                    swap { previousImplementation -> BlockSignature in
-                        { viewController, animated -> Void in
-                            previousImplementation(viewController, self.selector, animated)
-
-                            MyTraceLogger.stopTrace(associatedObject: viewController)
-                        }
-                    }
-                }
-            }
-
-        class ViewWillDisappear: MethodSwizzler<
-        @convention(c) (UIViewController, Selector, Bool) -> Void, // IMPSignature
-            @convention(block) (UIViewController, Bool) -> Void // BlockSignature
-            >
-            {
-                static func build() throws -> ViewWillDisappear {
-                    try ViewWillDisappear(selector: #selector(UIViewController.viewWillDisappear), klass: UIViewController.self)
-                }
-
-                func swizzle() {
-                    swap { previousImplementation -> BlockSignature in
-                        { viewController, animated -> Void in
-                            _ = MyTraceLogger.startTrace(tracer: ViewControllerInstrumentation.getTracer(), associatedObject: viewController, name: "\(type(of: viewController)).viewDisappear()", isRoot: true)
-                            previousImplementation(viewController, self.selector, animated)
-                        }
-                    }
-                }
-            }
-
-        class ViewWillLayoutSubviews: MethodSwizzler <
-        @convention(c) (UIViewController, Selector) -> Void,
-            @convention(block) (UIViewController) -> Void
-            >
-            {
-                static func build() throws -> ViewWillLayoutSubviews {
-                    try ViewWillLayoutSubviews(selector: #selector(UIViewController.viewWillLayoutSubviews), klass: UIViewController.self)
-                }
-
-                func swizzle() {
-                    swap { previousImplementation -> BlockSignature in
-                        { viewController -> Void in
-                           _ = MyTraceLogger.startTrace(tracer: ViewControllerInstrumentation.getTracer(), associatedObject: viewController, name: "\(type(of: viewController)).viewLayoutSubviews()", isRoot: false)
-                            previousImplementation(viewController, self.selector)
-                        }
-                    }
-                }
-            }
-
-        class ViewDidLayoutSubviews: MethodSwizzler <
-        @convention(c) (UIViewController, Selector) -> Void,
-            @convention(block) (UIViewController) -> Void
-            >
-            {
-                static func build() throws -> ViewDidLayoutSubviews {
-                    try ViewDidLayoutSubviews(selector: #selector(UIViewController.viewDidLayoutSubviews), klass: UIViewController.self)
-                }
-
-                func swizzle() {
-                    swap { previousImpelmentation -> BlockSignature in
-                        { viewController -> Void in
-                            previousImpelmentation(viewController, self.selector)
-                            MyTraceLogger.stopTrace(associatedObject: viewController)
-                        }
-                    }
-                }
-            }
+//        class ViewDidDisappear: MethodSwizzler<
+//        @convention(c) (UIViewController, Selector, Bool) -> Void, // IMPSignature
+//            @convention(block) (UIViewController, Bool) -> Void // BlockSignature
+//            >
+//            {
+//                static func build() throws -> ViewDidDisappear {
+//                    try ViewDidDisappear(selector: #selector(UIViewController.viewDidDisappear), klass: UIViewController.self)
+//                }
+//
+//                func swizzle() {
+//                    swap { previousImplementation -> BlockSignature in
+//                        { viewController, animated -> Void in
+//                            previousImplementation(viewController, self.selector, animated)
+//
+//                            MyTraceLogger.stopTrace(associatedObject: viewController)
+//                        }
+//                    }
+//                }
+//            }
+//
+//        class ViewWillDisappear: MethodSwizzler<
+//        @convention(c) (UIViewController, Selector, Bool) -> Void, // IMPSignature
+//            @convention(block) (UIViewController, Bool) -> Void // BlockSignature
+//            >
+//            {
+//                static func build() throws -> ViewWillDisappear {
+//                    try ViewWillDisappear(selector: #selector(UIViewController.viewWillDisappear), klass: UIViewController.self)
+//                }
+//
+//                func swizzle() {
+//                    swap { previousImplementation -> BlockSignature in
+//                        { viewController, animated -> Void in
+//                            _ = MyTraceLogger.startTrace(tracer: ViewControllerInstrumentation.getTracer(), associatedObject: viewController, name: "\(type(of: viewController)).viewDisappear()", isRoot: true)
+//                            previousImplementation(viewController, self.selector, animated)
+//                        }
+//                    }
+//                }
+//            }
+//
+//        class ViewWillLayoutSubviews: MethodSwizzler <
+//        @convention(c) (UIViewController, Selector) -> Void,
+//            @convention(block) (UIViewController) -> Void
+//            >
+//            {
+//                static func build() throws -> ViewWillLayoutSubviews {
+//                    try ViewWillLayoutSubviews(selector: #selector(UIViewController.viewWillLayoutSubviews), klass: UIViewController.self)
+//                }
+//
+//                func swizzle() {
+//                    swap { previousImplementation -> BlockSignature in
+//                        { viewController -> Void in
+//                           _ = MyTraceLogger.startTrace(tracer: ViewControllerInstrumentation.getTracer(), associatedObject: viewController, name: "\(type(of: viewController)).viewLayoutSubviews()", isRoot: false)
+//                            previousImplementation(viewController, self.selector)
+//                        }
+//                    }
+//                }
+//            }
+//
+//        class ViewDidLayoutSubviews: MethodSwizzler <
+//        @convention(c) (UIViewController, Selector) -> Void,
+//            @convention(block) (UIViewController) -> Void
+//            >
+//            {
+//                static func build() throws -> ViewDidLayoutSubviews {
+//                    try ViewDidLayoutSubviews(selector: #selector(UIViewController.viewDidLayoutSubviews), klass: UIViewController.self)
+//                }
+//
+//                func swizzle() {
+//                    swap { previousImpelmentation -> BlockSignature in
+//                        { viewController -> Void in
+//                            previousImpelmentation(viewController, self.selector)
+//                            MyTraceLogger.stopTrace(associatedObject: viewController)
+//                        }
+//                    }
+//                }
+//            }
     }
 
 #endif // #if os(iOS)
